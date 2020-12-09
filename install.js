751b82bd7c3b7b1d26c303bca3d4359792eff1d6
Joshua Peisach <itzswirlz2020@outlook.com>
2020-12-09 13:07:25 -0500

    Fix stability issues with install script

--- install.js
+++ install.js
@@ -1,5 +1,4 @@
-// IF THIS FAILS, BLAME NODE. Create the /usr/share/iheartradio-webapp directory, cleanup and try again.
-// I will fix stability problems later.
+// IF THIS FAILS, BLAME NODE. Ensure you ran npm install and npm run build first.
 const {exec} = require('child_process');
 
 // TODO: Allow for custom installdir's.
@@ -15,23 +14,31 @@ function createInstalldir() {
   });
 }
 
-console.log("Linking iHeartRadio binary file");
-exec("ln -sn " + installdir + "iheartradio /usr/bin/iheartradio", (error, stdout, stderr) => {
-  if (error) throw error; return;
-  if (stderr) throw stderr; return;
-  console.log('stdout: ${stdout}');
-});
+function installApp() {
+  // We do this because this way we also get .so libraries with it
+  console.log("Installing iHeartRadio");
+  exec("cp -r dist/linux-unpacked/* " + installdir, (error, stdout, stderr) => {
+    if (error) throw error; return;
+    if (stderr) throw stderr; return;
+    console.log('stdout: ${stdout}');
+  });
+}
+
+function linkBinary() {
+  console.log("Linking iHeartRadio binary file");
+  exec("ln -sn " + installdir + "iheartradio /usr/bin/iheartradio", (error, stdout, stderr) => {
+    if (error) throw error; return;
+    if (stderr) throw stderr; return;
+    console.log('stdout: ${stdout}');
+  });
+}
+
+createInstalldir();
+installApp();
+linkBinary();
 
 console.log("Installing desktop and menuentry file");
 exec("cp data/iheartradio.desktop /usr/share/applications/", (error, stdout, stderr) => {
-  if (error) throw error; return;
-  if (stderr) throw stderr; return;
-  console.log('stdout: ${stdout}');
-});
-
-// We do this because this way we also get .so libraries with it
-console.log("Installing iHeartRadio");
-exec("cp -r dist/linux-unpacked/* " + installdir, (error, stdout, stderr) => {
   if (error) throw error; return;
   if (stderr) throw stderr; return;
   console.log('stdout: ${stdout}');
